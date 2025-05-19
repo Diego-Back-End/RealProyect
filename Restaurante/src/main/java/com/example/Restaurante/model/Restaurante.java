d3303aca2ef2bea8f1724a02a7602fdcc8d4436b
escobedoambrosettidiego@gmail.com <escobedoambrosettidiego@gmail.com>
2025-05-18 23:31:35 -0400

    constructores y atributos de modelo restaurante

--- Restaurante/src/main/java/com/example/Restaurante/model/Restaurante.java
+++ Restaurante/src/main/java/com/example/Restaurante/model/Restaurante.java
@@ -1,5 +1,25 @@
 package com.example.Restaurante.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "restaurantes")
 public class Restaurante {
 
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String direccion;
+
+    private String telefono;
+
+    public Restaurante() {}
+
+    public Restaurante(String nombre, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
