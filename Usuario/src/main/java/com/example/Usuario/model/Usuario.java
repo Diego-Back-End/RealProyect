98ec9da5dcb715fc0e5af48cade739a89291cebe
escobedoambrosettidiego@gmail.com <escobedoambrosettidiego@gmail.com>
2025-05-18 23:20:04 -0400

    creando getters y setters en model

--- Usuario/src/main/java/com/example/Usuario/model/Usuario.java
+++ Usuario/src/main/java/com/example/Usuario/model/Usuario.java
@@ -1,5 +1,29 @@
 package com.example.Usuario.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String email;
+
+    //constructor vacio obligatorio para jpa
+    public Usuario(){}
+
+    public Usuario(String nombre,String email){
+        this.nombre = nombre;
+        this.email = email;
+    }
 
 }
